5f038ecb758f445f1f6ae52ab82251aab0eae42e
Larionov Vadim <larionov.vadim@mail.ru>
2015-02-15 19:35:34 +0300

    Добавлены методы getRequestMethod(), getRequestPath()

--- src/SocketManager.java
+++ src/SocketManager.java
@@ -1,5 +1,9 @@
 import java.io.*;
 import java.net.Socket;
+import java.net.URLDecoder;
+import java.nio.ByteBuffer;
+import java.nio.*;
+import java.nio.channels.AsynchronousSocketChannel;
 
 /**
  * Created by Vadim on 15.02.15.
@@ -8,6 +12,8 @@ public class SocketManager implements Runnable {
     private Socket socket;
     private InputStream inputStream;
     private OutputStream outputStream;
+
+    private int bufferSize = 8192;
 
     SocketManager(Socket socket) throws IOException {
         this.socket = socket;
@@ -18,7 +24,14 @@ public class SocketManager implements Runnable {
     public void run() {
         System.out.println("Method run()");
         try {
-            readInputHeaders();
+            String request = readInputHeaders();
+
+            String requestMethod = getRequestMethod(request);
+            System.out.println("Request Method: " + requestMethod);
+
+            String requestPath = getRequestPath(request);
+            System.out.println("Request Path: " + requestPath);
+
             String str = "<html><body><h1>Simple Http Server</h1></body></html>\n";
             writeResponse(str);
         } catch (IOException e) {
@@ -33,18 +46,22 @@ public class SocketManager implements Runnable {
         System.err.println("Client processing finished");
     }
 
-    private void readInputHeaders() throws IOException {
+    private String readInputHeaders() throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        String request = new String();
         while (true) {
             String header = bufferedReader.readLine();
+            request += header;
+            System.out.println(header);
             if (header == null || header.trim().length() == 0) {
                 break;
             }
         }
+        return request;
     }
 
     private void writeResponse(String message) throws IOException {
-        String headers = "HTTP/1.1 OK\r\n" +
+        String headers = "HTTP/1.1 200 OK\r\n" +
                 "Server: LarionovServer\r\n" +
                 "Content-Type: text/html\r\n" +
                 "Content-Length: " + message.length() + "\r\n" +
@@ -52,6 +69,31 @@ public class SocketManager implements Runnable {
         String response = headers + message;
         outputStream.write(response.getBytes());
         outputStream.flush();
+    }
+
+    private String getRequestMethod(String request) throws UnsupportedEncodingException {
+        int endIndex = request.indexOf(" ");
+        return  (endIndex != 0) ? request.substring(0, endIndex) : null;
+    }
+
+    private String getRequestPath(String request) {
+        int beginPath = request.indexOf(" ") + 1;
+        int endPath = request.indexOf(" ", beginPath);
+        // Какие могут быть ошибки?
+
+        String path = request.substring(beginPath, endPath);
+        try {
+            path = URLDecoder.decode(path, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        int queryIndex = path.indexOf("?");
+        System.out.println(queryIndex);
+        if (queryIndex != -1)
+            path = path.substring(0, queryIndex);
+
+        return path;
     }
 
 }
