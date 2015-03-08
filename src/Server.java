174893fab518d3cbf37b5c181be05aa3d1e07147
Larionov Vadim <larionov.vadim@mail.ru>
2015-03-08 22:59:05 +0300

    Добавлен Common CLI парсер аргументов командной строки

--- src/Server.java
+++ src/Server.java
@@ -4,6 +4,7 @@ import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import org.apache.commons.cli.*;
 
 /**
  * Created by vadim on 15.02.15.
@@ -18,7 +19,6 @@ public class Server {
     }
 
     public void run() throws Exception {
-        System.out.println("Netty Web Server");
         EventLoopGroup bossGroup = new NioEventLoopGroup();
         EventLoopGroup workerGroup = new NioEventLoopGroup();
         try {
@@ -47,14 +47,63 @@ public class Server {
     }
 
     public static void main(String[] args) throws Exception {
-        String inetHost = "127.0.0.1";
-        int port = 8030;
-        String documentRoot = "/home/vadim/http-test-suite";
+        String host = "127.0.0.1";
+        int port = 80;
+        String documentRoot = null;
 
-        Settings.setInetHost(inetHost);
+        Options posixOptions = getPosixOptions();
+        try {
+            CommandLineParser cmdLinePosixParser = new PosixParser();
+            CommandLine cmdLine = cmdLinePosixParser.parse(posixOptions, args);
+
+            if (cmdLine.hasOption("host")) {
+                host = cmdLine.getOptionValue("host");
+            }
+
+            if (cmdLine.hasOption("port")) {
+                port = Integer.valueOf(cmdLine.getOptionValue("port"));
+            }
+
+            if (cmdLine.hasOption("rootdir")) {
+                documentRoot = cmdLine.getOptionValue("rootdir");
+            }
+        }
+        catch (Exception e) {
+            HelpFormatter helpFormatter = new HelpFormatter();
+            helpFormatter.printHelp(" ", posixOptions);
+            return;
+        }
+
+        Settings.setInetHost(host);
         Settings.setPort(port);
         Settings.setDocumentRoot(documentRoot);
 
+        System.out.println("LarionovServer has been started at " + host + ":" + port);
         new Server(Settings.getInetHost(), Settings.getPort()).run();
     }
+
+    private static Options getPosixOptions() {
+        Option hostOption = new Option("b", "host", true, "Host");
+        hostOption.setArgs(1);
+        hostOption.setOptionalArg(true);
+        hostOption.setArgName("host");
+
+        Option portOption = new Option("p", "port", true, "Port");
+        portOption.setArgs(1);
+        portOption.setOptionalArg(true);
+        portOption.setArgName("port");
+
+        Option rootDirOption = new Option("r", "rootdir", true, "Document Root");
+        rootDirOption.setArgs(1);
+        rootDirOption.setRequired(true);
+        rootDirOption.setArgName("DocumentRoot");
+
+        Options posixOptions = new Options();
+        posixOptions.addOption(hostOption);
+        posixOptions.addOption(portOption);
+        posixOptions.addOption(rootDirOption);
+
+        return posixOptions;
+    }
+
 }
